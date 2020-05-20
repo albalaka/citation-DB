255433bd092c941e3ca4c1863322696913199119
Alon Albalak <alonalbalak@gmail.com>
2020-05-20 13:10:40 -0700

    added new citations scholarly is being updated, minor changes in code

--- citation.py
+++ citation.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 
 import sys
-import scholarly
+from scholarly import scholarly
 import time
 from tqdm import tqdm
 import pickle as pkl
 
+from stem import Signal
+from stem.control import Controller
+import requests
+
 proxies = {'http': 'socks5://127.0.0.1:9050',
            'https': 'socks5://127.0.0.1:9050'}
-scholarly.scholarly.use_proxy(**proxies)
+scholarly.use_proxy(**proxies)
+
+
+def refresh_socket():
+    print(requests.get('https://ident.me', proxies=proxies).text)
+    with Controller.from_port(port=9051) as c:
+        c.authenticate()
+        c.signal(Signal.NEWNYM)
+    print(requests.get('https://ident.me', proxies=proxies).text)
 
 
 # RL - reinforcement learning
@@ -17,16 +29,31 @@ scholarly.scholarly.use_proxy(**proxies)
 # NLI - natural language inference (same as recognizing textual entailment)
 # MC - machine comphrehension
 # MT - machine translation
+# SDS - Spoken dialogue system
+all_tags = ["ML",
+            "RL", "genetic programming", "meta learning", "game theory", "regret minimization",
+            "multi agent", "multi task",
 
-all_tags = ["ML", "RL", "CF", "meta learning", "transfer learning",
-            "multi-task", "continual learning",
-            "logic", "transformer", "multi agent",
-            "game theory", "regret minimization",
-            "relational reasoning", "QA", "inductive learning", "symbolic",
-            "CV", "image captioning", "NLP", "ASR", "SLU",
-            "sentence pair modeling", "STS", "paraphrase identification",
-            "NLI", "MC", "MT", "IR", "XAI",
-            "genetic programming", "VQA", "LM"]
+            "continual learning", "CF",
+            "transfer learning",
+
+            "IR",
+            "logic",  "symbolic", "relational reasoning", "inductive learning",
+
+            "transformer",
+
+            "NLP", "LM", "NLG",
+            "QA", "QG", "STS", "paraphrase identification",
+            "NLI", "MC", "MT", "abstractive summarization",
+
+            "VQA",
+
+            "CV", "image captioning",
+
+            "SLU", "ASR", "SDS",
+
+            "XAI",
+            ]
 
 
 def slow_down(i):
@@ -39,21 +66,27 @@ def slow_down(i):
 
 class publication_query(object):
     def __init__(self, query):
-        slow_down(10)
-        self.search_result = scholarly.search_pubs_query(query)
-        slow_down(10)
+        # slow_down(10)
+        self.search_result = scholarly.search_pubs(query)
+        # slow_down(10)
 
     def next_result(self):
-        slow_down(10)
-        result = next(self.search_result).fill()
+        # slow_down(10)
+        try:
+            result = next(self.search_result).fill()
+        except Exception:
+            print("Try again in an hourish")
+            print(time.strftime("%H:%M:%S", time.localtime()))
+            return None
         print(result)
-        slow_down(10)
+        # slow_down(10)
         return result
 
 
 class publication(object):
     def __init__(self, pub, read=False, notes=None, tags=None):
         self.bib = pub.bib
+        self.bib['title'] = self.bib['title'].replace('"', "'")
         self.cited_by = []
         self.cites_to = []
         self.read = read
@@ -70,10 +103,6 @@ class publication(object):
         except:
             self.ID = pub.url_scholarbib
         citedby = pub.get_citedby()
-        # for c in tqdm(citedby, total=pub.citedby):
-        #     time.sleep(30)
-        #     self.cited_by.append(c['id_scholarcitedby'])
-        #     time.sleep(30)
         for c in citedby:
             try:
                 self.cited_by.append(c.id_scholarcitedby)
@@ -94,6 +123,10 @@ class publication(object):
         if tag in self.tags:
             return True
         return False
+
+    def view(self):
+        print("TAGS: {}".format(self.tags))
+        print("Notes: {}".format(self.notes))
 
 
 class citation_DB(object):
@@ -152,24 +185,47 @@ class citation_DB(object):
                         cites_to.append(
                             [self.citations[c]['title'], self.citations[c]['publication'].tags])
 
-            print(paper['title'])
-            print(paper_has_tag)
+            # print(paper['title'])
+            # print(paper_has_tag)
             if paper_has_tag:
-                print("{}\n\thas tags: {}".format(paper['publication'].tags))
+                print("{}\n\thas tags: {}".format(
+                    paper['title'], paper['publication'].tags))
             elif citation_has_tag:
                 for title, tags in cites_to:
                     print("\t--->{} has tags: {}".format(title, tags))
             if paper_has_tag or citation_has_tag:
                 print()
 
+    def view_untagged(self):
+        print("Untagged Papers")
+        for ID, paper in self.citations.items():
+            if not paper['publication'].tags:
+                print(paper['title'])
+
     def view_all_citations(self):
         for paper in self.citations.values():
             print(paper['title'])
+            if paper['publication'].tags:
+                print("\t{}".format(paper['publication'].tags))
 
-    # def view_single(self, title):
-    #     found = False
-    #     for paper in self.citations.values():
-    #         if
+    def add_tags_notes_to_publication(self, title, tags, notes):
+        found = False
+        for paper in self.citations.values():
+            if paper['title'].lower() == title.lower():
+                paper['publication'].add_tags(tags)
+                paper['publication'].add_notes(notes)
+                save_DB(self)
+                found = True
+                print("Updated {}".format(paper['title']))
+        if not found:
+            print()
+            print("{} NOT FOUND IN DB".format(title))
+            print()
+
+    def view_single(self, title):
+        for paper in self.citations.values():
+            if paper['title'].lower() == title.lower():
+                paper['publication'].view()
 
 
 def save_DB(DB, save_path='citation_DB.pkl'):
@@ -200,22 +256,24 @@ def view_DB_citation_network_with_tag(tag, load_path="citation_DB.pkl"):
 
 def add_publication_from_GS(query, DB_path="citation_DB.pkl", tags=None, notes=None, read=False):
     DB = load_DB(DB_path)
-
+    refresh_socket()
     pubs = publication_query(query.lower())
     correct_result = False
     while not correct_result:
-        slow_down(10)
+        # slow_down(10)
         res = pubs.next_result()
+        if not res:
+            return
         answer = None
         while answer not in ("yes", "no"):
             answer = input("Enter yes or no: ")
             if answer == "yes":
                 correct_result = True
-        slow_down(10)
+        # slow_down(10)
 
-    slow_down(10)
+    # slow_down(10)
     pub = publication(res, read=read, notes=notes, tags=tags)
-    slow_down(10)
+    # slow_down(10)
     DB.add_publication(pub)
     save_DB(DB, save_path=DB_path)
-    slow_down(1200+(len(pub.cited_by)//2))
+    # slow_down(1200+(len(pub.cited_by)//2))
